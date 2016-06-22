f95fa1bd7777ae143ec3f2d2aa9fa91fe852f1db
andytree <632622435@qq.com>
2016-06-22 16:28:56 +0800

    Revert "add ex1-ex3"
    This reverts commit e4f421ffccbbfbda7ea106d74031be9abd085c65.

--- ex2.py
+++
@@ -1,7 +0,0 @@
-# -*-coding: utf-8 -*-
-# A comment, this is so you can read your program later.
-# Any thing afterthis # is ignored by ptyhon
-print "I could have code like this ."
-# You can alse use a comment to  "disable" or comment out a
-#print "this won't run"
-print "this will run."
